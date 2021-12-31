7103a6c83826cc53fb853d07585e1b0eb21609d5
Md. Rasel Khan <77205275+kmraselbd@users.noreply.github.com>
2021-12-31 18:51:31 +0600

    Add files via upload

---
+++ cuda project/resources/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    //STICKY MENU
+    $(".js--services-section").waypoint(function(direction){
+        if (direction == "down"){
+            $("nav").addClass("sticky");
+        } else {
+            $("nav").removeClass("sticky");
+        }
+    })
+
+    //MIXITUP (PORTFOLIO SECTION)
+    var mixer = mixitup(".container")
+});
